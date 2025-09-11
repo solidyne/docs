07ed17e87f4d6ed2da17de7ba3c70ff78e03cfdb
solidyne <118279928+solidyne@users.noreply.github.com>
2025-09-11 16:53:11 -0300

    Documentation edits made through Mintlify web editor

--- scripts/agent.js
+++ scripts/agent.js
@@ -1,6 +1,6 @@
 
 const agentInitScript = document.createElement("script");
 agentInitScript.type = "module";
-agentInitScript.innerHTML = `import Agent from 'https://rispose.com/cdn/v1/sdk.es.js'
+agentInitScript.innerHTML = `import Agent from 'https://rispose.com/cdn/v1/sdk.es.js';
 const agent = Agents.getOrCreate('ag_usybetbfuygk') ;
-
+document.body.append(agentInitScript);
