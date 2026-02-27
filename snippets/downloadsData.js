cfec8dfec1bd71ae5d47e1b07feb35478d13b0a3
pablospinelli <pablospin@gmail.com>
2026-02-27 20:25:51 -0300

    test para carga de listado de downloads

--- snippets/downloadsData.js
+++ snippets/downloadsData.js
@@ -1,1 +1,11 @@
-export default ["test"];
+export const downloads = [
+  {
+    product: "TEST",
+    name: "Test component",
+    description: "Desc",
+    platform: "Windows",
+    version: "1.0",
+    date: "",
+    url: "#"
+  }
+];
