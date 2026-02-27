4ef4b76514870f13f46d19f99f4b66ce5e8d0ad9
pablospinelli <pablospin@gmail.com>
2026-02-27 19:51:38 -0300

    downloads

---
+++ downloadsData - Copy.js
@@ -0,0 +1,167 @@
+export const downloads = {
+  unidex_ux24: {
+    product_name: "UNIDEX UX24",
+    downloads: [
+      {
+        component: "Drivers USB 4x4",
+        category: "driver",
+        version: null,
+        date: null,
+        platform: "Windows",
+        description: "Controladores para el dispositivo USB integrado (no aplica a UDX USB)",
+        url: "http://solidynepro.com/DW/Solidyne_UsbAudio.zip"
+      },
+      {
+        component: "Solidyne Multi-Discovery",
+        category: "software",
+        version: null,
+        date: null,
+        platform: "Windows",
+        description: "Herramienta que permite encontrar equipos Solidyne conectados a la red.",
+        url: "http://www.solidynepro.com/DW/IP.exe"
+      }
+    ]
+  },
+
+  unidex_ux18: {
+    product_name: "UNIDEX UX18",
+    downloads: [
+      {
+        component: "Drivers USB 4x4",
+        category: "driver",
+        version: null,
+        date: null,
+        platform: "Windows",
+        description: "Controladores para el dispositivo USB integrado (no aplica a UDX USB)",
+        url: "http://solidynepro.com/DW/Solidyne_UsbAudio.zip"
+      },
+      {
+        component: "Solidyne Multi-Discovery",
+        category: "software",
+        version: null,
+        date: null,
+        platform: "Windows",
+        description: "Herramienta que permite encontrar equipos Solidyne conectados a la red.",
+        url: "http://www.solidynepro.com/DW/IP.exe"
+      }
+    ]
+  },
+ procesador_542_apc: {
+    product_name: "Procesador 542 APC",
+    downloads: [
+      {
+        component: "Solidyne Multi-Discovery",
+        category: "software",
+        version: null,
+        date: null,
+        platform: "Windows",
+        description: "Herramienta que permite encontrar equipos Solidyne conectados a la red.",
+        url: "http://www.solidynepro.com/DW/IP.exe"
+      },
+      {
+        component: "Lite Commander",
+        category: "software",
+        version: null,
+        date: null,
+        platform: "Windows",
+        description: "Aplicación para automatización y control remoto del procesador vía red.",
+        url: "http://www.solidynepro.com/DW/lite.rar"
+      },
+      {
+        component: "Solidyne Discovery & Update",
+        category: "software",
+        version: "2",
+        date: null,
+        platform: "Windows",
+        description: "Herramienta para actualización del firmware. Ver documentación.",
+        url: "http://www.solidynepro.com/DW/setupDiscovery2.rar"
+      }
+    ]
+  },
+  audicom_14_black: {
+    product_name: "Audicom 14 Black",
+    downloads: [
+      {
+        component: "Actualización Audicom 14",
+        category: "update",
+        version: "14.02",
+        date: "2025-11",
+        platform: "Windows 8/10/11 64-bit",
+        description: "A partir de esta versión se requiere una edición de Windows de 64 bits.",
+        url: "https://www.solidynepro.com/Upgrade/setup1402_Upgrade.rar"
+      },
+      {
+        component: "Audicom 14 - Imagen ISO",
+        category: "iso",
+        version: null,
+        date: null,
+        platform: "Windows 7/8/10/11",
+        description: "Imagen ISO para instalación en máquina virgen. Requiere licencia actualizada.",
+        url: "https://www.solidynepro.com/DW/Audicom_LAST.iso"
+      }
+    ]
+  },
+  audicom_12: {
+    product_name: "Audicom 12",
+    downloads: [
+      {
+        component: "Actualización Audicom 12 Multicast",
+        category: "update",
+        version: "12.40",
+        date: null,
+        platform: "Windows 7/8/10/11",
+        description: "Actualización Multicast para Audicom 12.",
+        url: "https://www.solidynepro.com/Upgrade/setup1240_Upgrade.zip"
+      }
+    ]
+  },
+  audicom_11: {
+    product_name: "Audicom 11",
+    downloads: [
+      {
+        component: "Audicom 11 - Imagen ISO",
+        category: "iso",
+        version: "11.05",
+        date: null,
+        platform: "Windows",
+        description: "Imagen ISO de instalación.",
+        url: "https://www.solidynepro.com/DW/audicom1105.iso"
+      }
+    ]
+  },
+  admicom_14: {
+    product_name: "ADmicom 14",
+    downloads: [
+      {
+        component: "ADmicom 14 - Imagen ISO",
+        category: "iso",
+        version: "14.05",
+        date: "2025-02",
+        platform: "Windows",
+        description: "Renueva la interfaz de usuario simplificando operaciones y manteniendo compatibilidad con ADmicom 10-11. Incluye limpieza de contratos antiguos, optimización de base de datos, editor de reportes integrado y nuevas funciones.",
+        url: "https://www.solidynepro.com/Upgrade/SetupAdmicom_1405.rar"
+      }
+    ]
+  },
+  ada102: {
+    product_name: "ADA102",
+    downloads: [
+      {
+        component: "Firmware STL",
+        category: "firmware",
+        version: "3.06",
+        date: null,
+        description: "Firmware para transmitir y recibir entre dos equipos por streaming RTP.",
+        url: "https://www.solidynepro.com/Upgrade/ADA102-STL-306.rar"
+      },
+      {
+        component: "Firmware Streamer Shoutcast/Icecast",
+        category: "firmware",
+        version: "4.03",
+        date: null,
+        description: "Firmware para transmitir a servidores Icecast o Shoutcast.",
+        url: "https://www.solidynepro.com/Upgrade/ADA102_Streamer_403.rar"
+      }
+    ]
+  }
+};
