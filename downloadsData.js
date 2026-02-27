ac59e3b342b0739de4ef07e59e516f1ac63c07d8
solidyne <118279928+solidyne@users.noreply.github.com>
2026-02-27 15:51:40 -0300

    Refactor downloads data to separate file and simplify display

---
+++ downloadsData.js
@@ -0,0 +1,13 @@
+const downloads = [
+  {
+    product: "TEST PRODUCT",
+    name: "Test Component",
+    description: "Test description",
+    platform: "Windows",
+    version: "1.0",
+    date: "",
+    url: "#"
+  }
+];
+
+export default downloads;
